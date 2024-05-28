69c6237cb0741e6e7cb92e7d52f4837add890f5f
Joaogalescky <joaocvgalescky@gmail.com>
2024-05-28 14:59:01 -0300

    Definindo a lógica da aplicação

--- 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java-erudio/src/main/java/com/joaogalescky/MathController.java
+++ 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java-erudio/src/main/java/com/joaogalescky/MathController.java
@@ -13,9 +13,23 @@ public class MathController {
 	private final AtomicLong counter = new AtomicLong();
 
 	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-	public Double sum(
-			@PathVariable(value = "numberOne") String numberOne,
-			@PathVariable(value = "numberTwo") String numberTwo) {
-		return 1D;
+	public Double sum(@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new Exception();
+		}
+
+		return convertToDouble(numberOne) + convertToDouble(numberTwo);
+	}
+
+	private boolean isNumeric(String strNumber) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private Double convertToDouble(String strNumber) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
