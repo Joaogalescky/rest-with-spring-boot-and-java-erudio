874c7fb08b3934e7fd0abf66829ac1050432ac87
Joaogalescky <joaocvgalescky@gmail.com>
2024-05-31 15:43:11 -0300

    Content Negotiation via HEADER Parameter

--- 13_ContentNegotiation/rest-with-spring-boot-and-java-erudio/src/main/java/com/joaogalescky/config/WebConfig.java
+++ 13_ContentNegotiation/rest-with-spring-boot-and-java-erudio/src/main/java/com/joaogalescky/config/WebConfig.java
@@ -8,10 +8,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+	// Via QUERT PARAM - http://localhost:8080/api/person/v1?mediaType=xml
+	/*
+	 * @Override public void
+	 * configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+	 * configurer.favorParameter(true).parameterName("mediaType").ignoreAcceptHeader
+	 * (true) .useRegisteredExtensionsOnly(false).defaultContentType(MediaType.
+	 * APPLICATION_JSON) .mediaType("json",
+	 * MediaType.APPLICATION_JSON).mediaType("xml", MediaType.APPLICATION_XML); }
+	 */
+
+	// Via HEADER PARAM - http://localhost:8080/api/person/v1
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-		configurer.favorParameter(true).parameterName("mediaType").ignoreAcceptHeader(true)
-				.useRegisteredExtensionsOnly(false).defaultContentType(MediaType.APPLICATION_JSON)
-				.mediaType("json", MediaType.APPLICATION_JSON).mediaType("xml", MediaType.APPLICATION_XML);
+		configurer.favorParameter(false).ignoreAcceptHeader(false).useRegisteredExtensionsOnly(false)
+				.defaultContentType(MediaType.APPLICATION_JSON).mediaType("json", MediaType.APPLICATION_JSON)
+				.mediaType("xml", MediaType.APPLICATION_XML);
 	}
+
 }
