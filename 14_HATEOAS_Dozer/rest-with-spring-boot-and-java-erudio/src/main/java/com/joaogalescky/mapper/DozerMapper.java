2fc068377c81017655fa0d9b89f5b1b8f7b2c5ad
Joaogalescky <joaocvgalescky@gmail.com>
2024-06-03 14:18:51 -0300

    Inclusão de configuração comentada de ModelMapper

--- 14_HATEOAS_Dozer/rest-with-spring-boot-and-java-erudio/src/main/java/com/joaogalescky/mapper/DozerMapper.java
+++ 14_HATEOAS_Dozer/rest-with-spring-boot-and-java-erudio/src/main/java/com/joaogalescky/mapper/DozerMapper.java
@@ -6,11 +6,15 @@ import java.util.List;
 //import org.modelmapper.ModelMapper;
 import com.github.dozermapper.core.DozerBeanMapperBuilder;
 import com.github.dozermapper.core.Mapper;
+import com.joaogalescky.model.Person;
 
 public class DozerMapper {
 
 	// private static ModelMapper mapper = new ModelMapper();
-	
+
+	// static { mapper.createTypeMap(Person.class,
+	// PersonVO.class).addMapping(Person::getId, Person::setKey); }
+
 	private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
 
 	public static <O, D> D parseObject(O origin, Class<D> destination) {
