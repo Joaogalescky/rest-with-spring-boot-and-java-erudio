f270fd15f9e65f69260ddbe31fe15e020ef36d09
Joaogalescky <joaocvgalescky@gmail.com>
2024-06-04 20:39:04 -0300

    Criando um novo endpoint de books

--- 15_WorkingWithSwaggerOpenAPI/rest-with-spring-boot-and-java-erudio/src/main/java/com/joaogalescky/controllers/PersonController.java
+++ 15_WorkingWithSwaggerOpenAPI/rest-with-spring-boot-and-java-erudio/src/main/java/com/joaogalescky/controllers/PersonController.java
@@ -26,7 +26,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController // ResponseBody + Controller
 @RequestMapping("/api/person/v1")
-@Tag(name = "People", description = "Endpoints for managing people")
+@Tag(name = "Person", description = "Endpoints for managing people")
 public class PersonController {
 
 	@Autowired // private PersonServices service = new PersonServices();
