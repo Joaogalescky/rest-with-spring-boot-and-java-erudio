9fbc0ed531a8d8fc6d99d5734874ca37cee69150
Joaogalescky <joaocvgalescky@gmail.com>
2024-06-13 17:44:55 -0300

    Refactor: atualização, correção de erros e estilização

--- 15_WorkingWithSwaggerOpenAPI/rest-with-spring-boot-and-java-erudio/src/main/java/com/joaogalescky/config/OpenApiConfig.java
+++ 15_WorkingWithSwaggerOpenAPI/rest-with-spring-boot-and-java-erudio/src/main/java/com/joaogalescky/config/OpenApiConfig.java
@@ -11,8 +11,8 @@ import io.swagger.v3.oas.models.info.License;
 public class OpenApiConfig {
 
 	@Bean
-	public OpenAPI customOpenAPI() {
-		return new OpenAPI().info(new Info().title("RESTful API with Java 21 and Spring Boot 3").version("v1")
+	OpenAPI customOpenAPI() {
+		return new OpenAPI().info(new Info().title("RESTful API with Java 21 and Spring Boot 3.2.0").version("v1")
 				.description("Some description about your API").termsOfService("http://pub.erudio.com.br/meus-cursos")
 				.license(new License().name("Apache 2.0").url("http://pub.erudio.com.br/meus-cursos")));
 	}
