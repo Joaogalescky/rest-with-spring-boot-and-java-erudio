dd1c4bc39efe6d3552b78c541288b315a22acd19
Joaogalescky <joaocvgalescky@gmail.com>
2024-06-14 18:34:44 -0300

    Habilitando CORS de forma global

--- 16_ConfiguringCORS/rest-with-spring-boot-and-java-erudio/src/main/java/com/joaogalescky/config/WebConfig.java
+++ 16_ConfiguringCORS/rest-with-spring-boot-and-java-erudio/src/main/java/com/joaogalescky/config/WebConfig.java
@@ -2,10 +2,12 @@ package com.joaogalescky.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.joaogalescky.serialization.converter.YmalJackson2HttpMessageConverter;
@@ -15,6 +17,9 @@ public class WebConfig implements WebMvcConfigurer {
 
 	private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
 
+	@Value("${cors.originPatterns:default}")
+	private String corsOriginPatterns = "";
+	
 	// Via QUERT PARAM - http://localhost:8080/api/person/v1?mediaType=xml
 	/*
 	 * @Override public void
@@ -36,5 +41,15 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 		converters.add(new YmalJackson2HttpMessageConverter());
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		var allowedOrigins = corsOriginPatterns.split(",");
+		registry.addMapping("/**")
+		//.allowedMethods("GET", "POST", "PUT");
+		.allowedMethods("*")
+		.allowedOrigins(allowedOrigins)
+		.allowCredentials(true);
 	}
 }
