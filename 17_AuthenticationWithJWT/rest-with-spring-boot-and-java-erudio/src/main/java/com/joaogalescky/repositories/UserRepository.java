7071923afdd0ca14f24ffa31017208e450649ef6
Joaogalescky <joaocvgalescky@gmail.com>
2024-06-15 16:21:35 -0300

    Implementado o UserRepository e o UserService

---
+++ 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/com/joaogalescky/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.joaogalescky.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.joaogalescky.model.Person;
+import com.joaogalescky.model.User;
+
+@Repository // Opcional a partir da versão Spring Boot 3.0.1
+public interface UserRepository extends JpaRepository<User, Long> {
+	@Query("SELECT u FROM User WHERE u.userName =:userName")
+	User findByUsername(@Param("userName") String userName);
+}
