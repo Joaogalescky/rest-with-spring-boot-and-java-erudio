9e6e6e2af698f7733dc85479f088e82b2d4abee2
Joaogalescky <joaocvgalescky@gmail.com>
2024-06-15 16:43:23 -0300

    Implementado o JwtTokenProvider

---
+++ 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/com/joaogalescky/securityJwt/JwtTokenProvider.java
@@ -0,0 +1,67 @@
+package com.joaogalescky.securityJwt;
+
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.joaogalescky.data.vo.v1.security.TokenVO;
+
+import jakarta.annotation.PostConstruct;
+
+@Service
+public class JwtTokenProvider {
+
+	@Value("${security.jwt.token.secret-key:secret}")
+	private String secretKey = "secret";
+
+	@Value("${security.jwt.token.expire-length:3600000}")
+	private long validityInMilliseconds = 3600000; // 1h
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	Algorithm algorithm = null;
+
+	@PostConstruct
+	protected void init() {
+		secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+		algorithm = Algorithm.HMAC256(secretKey.getBytes());
+	}
+
+	public TokenVO createAccessToken(String username, List<String> roles) {
+		Date now = new Date();
+		Date validity = new Date(now.getTime() + validityInMilliseconds);
+		var accessToken = getAccessToken(username, roles, now, validity);
+		var refreshToken = getRefreshToken(username, roles, now);
+
+		return new TokenVO(username, true, now, validity, accessToken, refreshToken);
+	}
+
+	public TokenVO refreshToken(String refreshToken) {
+		if (refreshToken.contains("Bearer "))
+			refreshToken = refreshToken.substring("Bearer ".length());
+
+		JWTVerifier verifier = JWT.require(algorithm).build();
+		DecodedJWT decodedJWT = verifier.verify(refreshToken);
+		String username = decodedJWT.getSubject();
+		List<String> roles = decodedJWT.getClaim("roles").asList(String.class);
+		return createAccessToken(username, roles);
+	}
+
+	private String getAccessToken(String username, List<String> roles, Date now, Date validity) {
+		return null;
+	}
+
+	private String getRefreshToken(String username, List<String> roles, Date now) {
+		return null;
+	}
+}
