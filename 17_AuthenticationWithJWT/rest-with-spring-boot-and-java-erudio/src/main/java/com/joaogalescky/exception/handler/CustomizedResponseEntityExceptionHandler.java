26dfe39a008549e8b6f66da4854fe7ecdf092ec0
Joaogalescky <joaocvgalescky@gmail.com>
2024-06-15 16:29:41 -0300

    Criado exceptions costumizados e o VO de credenciais

--- 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/com/joaogalescky/exception/handler/CustomizedResponseEntityExceptionHandler.java
+++ 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/com/joaogalescky/exception/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.joaogalescky.exceptions.ExceptionResponse;
+import com.joaogalescky.exceptions.InvalidJwtAuthenticationException;
 import com.joaogalescky.exceptions.RequiredObjectIsNullException;
 import com.joaogalescky.exceptions.ResourceNotFoundException;
 
@@ -37,5 +38,15 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
 				request.getDescription(false));
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(InvalidJwtAuthenticationException.class)
+	public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationExceptions(Exception ex,
+			WebRequest request) {
+
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
+				request.getDescription(false));
+
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
 	}
 }
