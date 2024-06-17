4683352e1f87fa0644f66d8a3a83673d93ad2535
Joaogalescky <joaocvgalescky@gmail.com>
2024-06-17 11:47:25 -0300

    Refactor: alteração de valor de passwordEncoder

--- 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/com/joaogalescky/StartUp.java
+++ 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/com/joaogalescky/StartUp.java
@@ -1,14 +1,18 @@
 package com.joaogalescky;
 
-/*import java.util.HashMap;
-import java.util.Map;*/
+/*
+import java.util.HashMap;
+import java.util.Map;
+*/
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/*import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+/*
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;*/
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
+*/
 
 @SpringBootApplication
 public class StartUp {
@@ -16,10 +20,10 @@ public class StartUp {
 	public static void main(String[] args) {
 		SpringApplication.run(StartUp.class, args);
 
-		/**
+		/*
 		 * Map<String, PasswordEncoder> encoders = new HashMap<>();
 		 * 
-		 * Pbkdf2PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder( "", 8,
+		 * Pbkdf2PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder("", 8,
 		 * 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
 		 * 
 		 * encoders.put("pbkdf2", pbkdf2Encoder); DelegatingPasswordEncoder
--- 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
+++ 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
@@ -1,3 +1,3 @@
 ﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
-	('leandro', 'Leandro Costa', '1e3cdeeaaaeeda173ff6d002e7cb5e3f91ebc354dcff52156c9eaba1793a3a5e5bee306c11099e22', b'1', b'1', b'1', b'1'),
-	('flavio', 'Flavio Costa', '362ad02420268beeb22d3a1f0d92749df461d7f4b74c9433d7415bdeef1b2902f4eb1edaecb37cb3', b'1', b'1', b'1', b'1');
+	('leandro', 'Leandro Costa', 'ec7c0c72fdb847f7f845854036e2134dde54d39502ab8da1290c93c4aea9cabbf93bfde87e6893bd', b'1', b'1', b'1', b'1'),
+	('flavio', 'Flavio Costa', '659399420566e15ede16685fed927174de4b8015c5011d995f3580a7f8e9773fcbfdc48b76778259', b'1', b'1', b'1', b'1');
