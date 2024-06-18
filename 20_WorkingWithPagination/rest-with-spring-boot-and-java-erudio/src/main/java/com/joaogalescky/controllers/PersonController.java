0091cd824360af2b29bca4543c9c234085a08a6b
Joaogalescky <joaocvgalescky@gmail.com>
2024-06-18 09:46:30 -0300

    Implementado ordenação com Direction

--- 20_WorkingWithPagination/rest-with-spring-boot-and-java-erudio/src/main/java/com/joaogalescky/controllers/PersonController.java
+++ 20_WorkingWithPagination/rest-with-spring-boot-and-java-erudio/src/main/java/com/joaogalescky/controllers/PersonController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -60,10 +62,15 @@ public class PersonController {
 			@ApiResponse(description = "Unathorized", responseCode = "401", content = @Content),
 			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
 			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content) })
-	public ResponseEntity<Page<PersonVO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "limit", defaultValue = "12") Integer limit) {
-		Pageable pageable = PageRequest.of(page, limit);
+	public ResponseEntity<Page<PersonVO>> findAll(
+	//@formatter:off
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "limit", defaultValue = "12") Integer limit,
+			@RequestParam(value = "direction", defaultValue = "asc") String direction) {
+		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+		Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "firstName"));
 		return ResponseEntity.ok(service.findAll(pageable));
+	//@formatter:on
 	}
 
 	@CrossOrigin(origins = { "http://localhost:8080", "https://erudio.com.br" })
